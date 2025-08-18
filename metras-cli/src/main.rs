ff15f0db814aa5a0ab87b30698cf9b078a4704be
Kornelijus Tvarijanavičius <16837066+Kornelijus@users.noreply.github.com>
2025-08-18 23:41:58 +0300

    cleanup example docs from cli

--- metras-cli/src/main.rs
+++ metras-cli/src/main.rs
@@ -1,33 +1,12 @@
 //! An example to showcase how one can build an authenticated socks5 CONNECT proxy server.
 //!
-//! # Run the example
-//!
-//! ```sh
-//! cargo run --example socks5_connect_proxy --features=dns,socks5
-//! ```
-//!
 //! # Expected output
 //!
 //! The server will start and listen on `:62021`. You can use `curl` to interact with the service:
 //!
 //! ```sh
-//! curl -v -x socks5://127.0.0.1:62021 --proxy-user 'john:secret' http://www.example.com/
-//! curl -v -x socks5h://127.0.0.1:62021 --proxy-user 'john:secret' http://www.example.com/
 //! curl -v -x socks5://127.0.0.1:62021 --proxy-user 'john:secret' https://www.example.com/
 //! curl -v -x socks5h://127.0.0.1:62021 --proxy-user 'john:secret' https://www.example.com/
-//! ```
-//!
-//! You should see in all the above examples the responses from the server.
-//!
-//! In case you use wrong credentials you'll see something like:
-//!
-//! ```sh
-//! $ curl -v -x socks5://127.0.0.1:62021 --proxy-user 'john:foo' http://www.example.com/
-//! *   Trying 127.0.0.1:62021...
-//! * Connected to 127.0.0.1 (127.0.0.1) port 62021
-//! * User was rejected by the SOCKS5 server (1 1).
-//! * Closing connection
-//! curl: (97) User was rejected by the SOCKS5 server (1 1).
 //! ```
 
 use rama::{
